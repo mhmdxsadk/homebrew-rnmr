ca83f0c3d1eebc23cf2e1cd8179ded5488682d43
Muhammed Sadik <50425929+mhmdxsadk@users.noreply.github.com>
2025-11-09 15:07:17 +0000

    Homebrew: v1.0.4 (python@3.9)

--- Formula/rnmr.rb
+++ Formula/rnmr.rb
@@ -1,12 +1,26 @@
 class Rnmr < Formula
+  include Language::Python::Virtualenv
+
   desc "Lightweight command-line tool for renaming files and directories"
-  homepage "https://github.com/mhmdxsadk/rnmr"
-  url "https://github.com/mhmdxsadk/homebrew-rnmr/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "1a4b45f584e4e243127e4b166b755aa902b645e087f90b359e3cbec915db46c6"
+  homepage "https://github.com/mhmdxsadk/homebrew-rnmr"
+  url "https://github.com/mhmdxsadk/homebrew-rnmr/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "81579a4265fd7d56675f0f94fc31f668cb1d251c55b23048bc935ca2c8c7fd3d"
   license "MIT"
 
+  depends_on "python@3.9"
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/py3/c/click/click-8.1.7.tar.gz"
+    sha256 "ca9853ad28de1ce219aab4a3b0287a34cbf194ab86f8e4e9dcebc9f7ed2e8f6e"
+  end
+
+  resource "colorama" do
+    url "https://files.pythonhosted.org/packages/py3/c/colorama/colorama-0.4.6.tar.gz"
+    sha256 "08695f5cb7ed6a8a1e22d2b6e6e3e0c3b77d2e3a8bbf78f8c0f3f75f31a6f0b8"
+  end
+
   def install
-    bin.install "rnmr.py" => "rnmr"
+    virtualenv_install_with_resources
   end
 
   test do
@@ -16,4 +30,3 @@ class Rnmr < Formula
     assert_equal "content", (testpath/"y.txt").read.strip
   end
 end
-
